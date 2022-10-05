fc9d8fcbe1e020d7b5e7fe5622d2e35c2199d949
SoS1ckkk <90698553+SoS1ckkk@users.noreply.github.com>
2022-10-06 00:04:53 +0700

    Add files via upload

---
+++ PERTEMUAN 3 DAN 4/ObjectMovement.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    Vector3 maju;
+    Vector3 mundur;
+    Vector3 keatas;
+    Vector3 kebawah;
+    Vector3 kiri;
+    Vector3 kanan;
+
+    public int speed;
+    public int speedRotasi;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maju = new Vector3(1, 0, 0);
+        mundur = new Vector3(-1, 0, 0);
+        keatas = new Vector3(0, 1, 0);
+        kebawah = new Vector3(0, -1, 0);
+        kiri = new Vector3(0, 0, 1);
+        kanan = new Vector3(0,0,-1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d")) {
+                transform.position = transform.position + (maju * speed * Time.deltaTime);
+        }
+            
+        if (Input.GetKey("a")) {
+                transform.position = transform.position + (mundur * speed * Time.deltaTime);
+            }
+
+        if (Input.GetKey("w")) {
+                transform.position = transform.position + (keatas * speed * Time.deltaTime);
+            }
+
+        if (Input.GetKey("s")) {
+                transform.position = transform.position + (kebawah * speed * Time.deltaTime);
+            }
+            
+        if (Input.GetKey("e")) {
+                transform.rotation = transform.rotation * Quaternion.Euler (kanan * speedRotasi * Time.deltaTime);
+            }
+            
+        if (Input.GetKey("q")) {
+                transform.rotation = transform.rotation * Quaternion.Euler (kiri * speedRotasi * Time.deltaTime);
+            }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "enemytag") {
+            Debug.Log("Game Over");
+            Time.timeScale = 0;
+
+        }
+
+        if (coll.gameObject.tag == "cointag") {
+            score += 1; 
+            Debug.Log("score = " + score);
+        }
+    
+    }
+
+    void OnCollisionStay2D(Collision2D coll) {
+        if (coll.gameObject.tag == "enemytag") {
+            Debug.Log("Sedang Nabrak!");
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll) {
+        if (coll.gameObject.tag == "enemytag") {
+            Debug.Log("Sudah Nabrak!");
+        }
+    }
+
+}
